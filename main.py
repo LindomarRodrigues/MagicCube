877fd466d80c40c6b15ff755fffeb8eb481c7cc1
LindomarRodrigues <lindomarrodriguesdeazevedo@outlook.com>
2023-11-20 10:26:03 -0300

    Adding CFOP solver to the use example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cube import Cube
 from solvers.layer_by_layer import LayerByLayerSolver
+from solvers.CFOP import CfopSolver
 from solvers.optimizer import optimize_moves
 
 cube = Cube()
@@ -10,13 +11,24 @@ solver.solve()
 
 moves = optimize_moves(solver.moves)
 
+print("Layer by layer:")
 print(f"Quantity of moves: {len(solver.moves)}")
 print(f"Quantity of optimized moves: {len(moves)}")
 
 print(f"Movements: {' '.join(moves)}")
 
-cube.moves("B U' B2 L2 D2 R2 D' L2 U2 B2 U R' F' L R' D F2 U' R'")
-cube.plot()
 
-cube.moves(moves)
-cube.plot()
+cube = Cube()
+cube.moves("B U' B2 L2 D2 R2 D' L2 U2 B2 U R' F' L R' D F2 U' R'")
+
+solver = CfopSolver(cube)
+solver.solve()
+
+moves = optimize_moves(solver.moves)
+
+print("\nCFOP: ")
+print(f"Quantity of moves: {len(solver.moves)}")
+print(f"Quantity of optimized moves: {len(moves)}")
+
+print(f"Movements: {' '.join(moves)}")
+
